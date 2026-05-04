a76a8b20896d528f785f6d0d68739e504b91cf4e
Maodo Ndiaye <stagiaire01@dicorpsas.com>
2026-05-04 16:11:33 +0000

    refactor: comment out ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import "./app.css"
 import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+// import { ThemeProvider } from "@/components/theme-provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
+    {/* <ThemeProvider> */}
       <App />
-    </ThemeProvider>
+    {/* </ThemeProvider> */}
   </StrictMode>
 )
